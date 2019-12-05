15ca3c767ac988575667aafc8145ec736a0fcad1
Aleksandr Staetsky <myexploretime@gmail.com>
2019-12-05 17:21:43 +0300

    hero section layout

--- src/assets/scripts/app.js
+++ src/assets/scripts/app.js
@@ -1,1 +1,12 @@
 
+import Swiper from 'swiper';
+
+let mySwiper = new Swiper('.swiper-container', {
+ pagination: {
+  el: '.swiper-pagination',
+  clickable: true,
+  renderBullet: function (index, className) {
+   return '<span class="' + className + '">' + (index + 1) + '</span>';
+  },
+ },
+});
